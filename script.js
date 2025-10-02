89eba01fe4dc709db15fad387e1ae8e8ec3f4370
Kaique <kaique1245br@gmail.com>
2025-10-02 18:40:54 -0300

    Portifólio finalizado

--- script.js
+++ script.js
@@ -47,3 +47,23 @@ function type() {
 }
 
 type();
+
+
+const sections = document.querySelectorAll('.section');
+
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            } else {
+                entry.target.classList.remove('visible'); // remove ao sair
+            }
+        });
+    },
+    {
+        threshold: 0.3, // 30% visível para ativar
+    }
+);
+
+sections.forEach(section => observer.observe(section));
